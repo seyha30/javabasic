9bde4696a1c461bc2a9939a6d5fb522d26d02e81
sseyha1 <sinseyha1@gmail.com>
2021-04-24 19:26:40 +0700

    push

---
+++ src/IFELSEIF/DayTranslate.java
@@ -0,0 +1,41 @@
+package IFELSEIF;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+public class DayTranslate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		Scanner in = new Scanner(System.in);
+//		System.out.println("Enter day of week: ");
+		String daysOfWeek = "";
+		String translate = "";
+		daysOfWeek  = JOptionPane.showInputDialog( null,"Enter day of week");
+		if("Monday".equalsIgnoreCase(daysOfWeek)) {
+			translate = "ថ្ងៃចន្ទ";
+		}		
+		else if("Tuesday".equalsIgnoreCase(daysOfWeek)) {
+			translate = "ថ្ងៃអង្គារ";
+		}		
+		else if("Wednesday".equalsIgnoreCase(daysOfWeek)) {
+			translate = "ថ្ងៃពុធ";
+		}		
+		else if("Thursday".equalsIgnoreCase(daysOfWeek)) {
+			translate = "ថ្ងៃព្រហស្បតិ៍្ត";
+		}		
+		else if("Friday".equalsIgnoreCase(daysOfWeek)) {
+			translate = "ថ្ងៃសុក្រ";
+		}		
+		else if("Saturday".equalsIgnoreCase(daysOfWeek)) {
+			translate = "ថ្ងៃសៅរ៍";
+		}
+		else if("Sunday".equalsIgnoreCase(daysOfWeek)) {
+			translate = "ថ្ងៃអាទិត្យ";
+		}
+		else {
+			translate = "មិនស្គាល់";
+		}
+		JOptionPane.showMessageDialog(null, translate);
+
+	}
+
+}
---
+++ src/IFELSEIF/TrafficLight.java
@@ -0,0 +1,14 @@
+package IFELSEIF;
+import java.util.Scanner;
+public class TrafficLight {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		String lightColor = "";
+		System.out.println("Please enter traffic light color :");
+		lightColor = in.next();
+
+	}
+
+}
